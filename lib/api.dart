c9d01673b341c85a759847231ed3710f35feceea
Aditya Dave <cool2912004@gmail.com>
2023-09-29 01:00:30 +0530

    "29-sep"

---
+++ lib/api.dart
@@ -0,0 +1,2 @@
+const LiveDataAPIKey =
+    'https://spoyer.com/api/en/get.php?login=ayna&token=12784-OhJLY5mb3BSOx0O&task=livedata&sport=soccer';
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:football_app/api.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> matches = [];
+
+  void fetchData() async {
+    const url = LiveDataAPIKey;
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      matches = json['games_live'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: const Color(0xFF2B303D),
+          title: Text(
+            'MATCHES',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontFamily: 'DIN Pro',
+              fontWeight: FontWeight.w900,
+              height: 0,
+            ),
+          )),
+      body: ListView.builder(
+        itemCount: matches.length,
+        itemBuilder: (context, index) {
+          final match = matches[index];
+          final team1 = match['league']['name'];
+          return ListTile(
+            title: Text(team1),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: fetchData),
+    );
+  }
+}
