23a39048d83544d1e24bbcff46b9489d622b8f93
Aditya Dave <107619782+adityadave29@users.noreply.github.com>
2023-10-22 22:13:08 +0530

    Update result.dart

--- lib/screens/result.dart
+++ lib/screens/result.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:football_app/screens/dice_roller.dart';
+import 'package:football_app/screens/home_screen_shape.dart';
 
 class Result extends StatefulWidget {
   const Result(
@@ -21,6 +24,15 @@ class _ResultState extends State<Result> {
 
   @override
   Widget build(BuildContext context) {
+    final random = Random();
+
+    // Generate a random number between 1 and 3
+    int number1 = 1 + random.nextInt(3);
+
+    // Generate a random number between 4 and 5
+    int number2 = 4 + random.nextInt(2);
+
+    int displayOne, displayTwo;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -56,84 +68,126 @@ class _ResultState extends State<Result> {
                       height: 0,
                     ),
                   ),
-                  Container(
-                    height: 100,
-                    width: 300,
-                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                    margin: EdgeInsets.symmetric(horizontal: 2, vertical: 5),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/ground.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Padding(
-                      // padding: EdgeInsets.only(left: )
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: (widget.flag == '1')
-                                ? Text(
-                                    'My Team',
-                                    softWrap: true,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontFamily: 'DIN Pro',
-                                      fontWeight: FontWeight.w900,
-                                    ),
-                                  )
-                                : Text(
-                                    '${widget.team1}',
-                                    softWrap: true,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontFamily: 'DIN Pro',
-                                      fontWeight: FontWeight.w900,
-                                    ),
-                                  ),
-                          ),
-                          Text(
-                            "-",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontFamily: 'DIN Pro',
-                              fontWeight: FontWeight.w900,
+                  Stack(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(40),
+                        height: 200,
+                        child: ClipPath(
+                          clipper: CustomClipPath(),
+                          child: Container(
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/ground.png'),
+                                fit: BoxFit.cover,
+                                opacity: 0.4,
+                              ),
                             ),
                           ),
-                          Expanded(
-                            child: (widget.flag == '2')
-                                ? Text(
-                                    'My Team',
-                                    softWrap: true,
-                                    textAlign: TextAlign.right,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontFamily: 'DIN Pro',
-                                      fontWeight: FontWeight.w900,
-                                    ),
-                                  )
-                                : Text(
-                                    '${widget.team2}',
-                                    softWrap: true,
-                                    textAlign: TextAlign.right,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontFamily: 'DIN Pro',
-                                      fontWeight: FontWeight.w900,
-                                    ),
-                                  ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                      Container(
+                        height: 130, // Match the height of the ClipPath
+                        //   width: double.infinity,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: (widget.flag == '1')
+                                  ? Container(
+                                      padding: EdgeInsets.only(
+                                          left: 70,
+                                          right: 1,
+                                          bottom: 5,
+                                          top: 65),
+                                      child: Text(
+                                        'My Team $number2',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize:
+                                              15, // Adjust the font size as needed
+                                          fontFamily: 'DIN Pro',
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                      ),
+                                    )
+                                  : Container(
+                                      padding: EdgeInsets.only(
+                                          left: 70, right: 1, top: 60),
+                                      child: Text(
+                                        '${widget.team1}  ${number1}',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize:
+                                              15, // Adjust the font size as needed
+                                          fontFamily: 'DIN Pro',
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                      ),
+                                    ),
+                            ),
+                            Container(
+                              margin:
+                                  EdgeInsets.only(bottom: 1, left: 15, top: 55),
+                              child: Text(
+                                "-",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize:
+                                      17, // Adjust the font size as needed
+                                  fontFamily: 'DIN Pro',
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: (widget.flag == '2')
+                                  ? Container(
+                                      padding: EdgeInsets.only(
+                                          left: 40,
+                                          right: 60,
+                                          // bottom: 5,
+                                          top: 60),
+                                      child: Text(
+                                        'My Team    ${number2}',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize:
+                                              15, // Adjust the font size as needed
+                                          fontFamily: 'DIN Pro',
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                      ),
+                                    )
+                                  : Container(
+                                      padding: EdgeInsets.only(
+                                          left: 30,
+                                          right: 40,
+                                          // bottom: 5,
+                                          top: 60),
+                                      child: Text(
+                                        '${widget.team2}              ${number1}',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize:
+                                              15, // Adjust the font size as needed
+                                          fontFamily: 'DIN Pro',
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                      ),
+                                    ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 10),
+                  // SizedBox(height: 10),
                   Container(
                     height: 80,
                     width: 80,
